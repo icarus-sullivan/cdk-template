24494e4d3bea7654ac14c14e8a4de3c8ae3878f5
Chris Sullivan <chrissullivan.dev@gmail.com>
2020-10-01 10:38:28 -0700

    Adding smart naming

--- stack.js
+++ stack.js
@@ -10,9 +10,6 @@ const path = require('path');
 
 const CONFIG = {
     name: 'exp',
-    vpcName: 'expVpc',
-    clusterName: 'expCluster',
-    fargateName: 'expFargate',
 
     dockerDirectory: path.resolve(__dirname, 'docker'),
 
@@ -26,8 +23,11 @@ const CONFIG = {
     env: {
         region: 'us-east-1',
         account: 'your_account_id',
+        stage: 'dev',
     }
 }
+
+const resolveName = (name, config) => [config.name, name, config.env.stage].join('-');
 
 const resolveZone = (domainName) => route53.HostedZone.fromLookup(this, 'ez', { 
     domainName,
@@ -39,18 +39,31 @@ const resolveCertificate = (arn) =>
 class CdkStack extends cdk.Stack {
     constructor(scope, id, props) {
         super(scope, id, props);
+
+        const { 
+            certificateArn, 
+            dockerDirectory,
+            domains: { 
+                hostedDomain, 
+                outputDomain 
+            } 
+        } = CONFIG;
+
+        const vpcName = resolveName('vpc', CONFIG);
+        const clusterName = resolveName('cluster', CONFIG);
+        const fargateName = resolveName('fargate', CONFIG);
   
-        const vpc = new ec2.Vpc(this, CONFIG.vpcName, { maxAzs: 3 });
-        const cluster = new ecs.Cluster(this, CONFIG.clusterName, { vpc });
+        const vpc = new ec2.Vpc(this, vpcName, { maxAzs: 3, vpcId: vpcName });
+        const cluster = new ecs.Cluster(this, clusterName, { vpc, clusterName });
 
-        const { hostedDomain, outputDomain } = CONFIG.domains;
         const existingZone = resolveZone(hostedDomain);
-        const certificate = resolveCertificate(CONFIG.certificateArn);
+        const certificate = resolveCertificate(certificateArn);
 
-        const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, CONFIG.fargateName, {
+        const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, fargateName, {
+            serviceName: fargateName,
             cluster,
             taskImageOptions: {
-                image: ecs.ContainerImage.fromAsset(CONFIG.dockerDirectory),
+                image: ecs.ContainerImage.fromAsset(dockerDirectory),
                 containerPort: 3000,
             },
             certificate,
